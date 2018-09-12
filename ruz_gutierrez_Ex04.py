aaad74dc935a83daf782610b6513b986dde71829
gordian-eggo <columbus.knot@gmail.com>
2018-09-12 17:24:24 +0800

    Exer04 up!

---
+++ ruz_gutierrez_Ex04.py
@@ -0,0 +1,18 @@
+'''
+RUZ, Julianne Marie					2014-04280
+GUTIERREZ, Reigner D. 				2014-06096
+CMSC 165 U-2L
+Exer 04: locate plate numbers in the given three images.
+'''
+
+import cv2
+import math
+import numpy as np
+
+def filter(image, kernel):
+	
+
+img1 = cv2.read()
+img2 = cv2.read()
+img3 = cv2.read()
+	
