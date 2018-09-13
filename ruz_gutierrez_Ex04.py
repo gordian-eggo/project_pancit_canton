105e04e0e26a4fed1cd82d324aae75153974949e
gordian-eggo <columbus.knot@gmail.com>
2018-09-13 12:31:37 +0800

    Trying to get the contour area

--- ruz_gutierrez_Ex04.py
+++ ruz_gutierrez_Ex04.py
@@ -9,10 +9,28 @@ import cv2
 import math
 import numpy as np
 
-def filter(image, kernel):
-	
+#def 
 
-img1 = cv2.read()
-img2 = cv2.read()
-img3 = cv2.read()
-	
+img = cv2.imread("p1.jpg", 0)
+ret, thresh = cv2.threshold(img, 127, 255, 0)
+contours = cv2.findContours(thresh, 1, 2)
+hierarchy = cv2.findContours(thresh, 1, 2)
+
+
+cnt = contours[0]
+M = cv2.moments(cnt)
+
+cx = int(M['m10']/M['m00'])
+cy = int(M['m01']/M['m00'])
+
+area = cv2.contourArea(cnt)
+# perimeter = cv2.
+print(area)
+
+'''
+
+ERROR LIST:
+
+	Line 26: cv2.error: OpenCV(3.4.2) /io/opencv/modules/imgproc/src/shapedescr.cpp:272: error: (-215:Assertion failed) npoints >= 0 && (depth == 5 || depth == 4) in function 'contourArea'
+
+'''
