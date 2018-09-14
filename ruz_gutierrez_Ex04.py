138a082dbe157ef8df0883361a8210d7c4a84cd5
gordian-eggo <columbus.knot@gmail.com>
2018-09-14 22:07:22 +0800

    mumsh ikaw na magconvert to colors. nag=iwan din ako ng error message sa baba ng codes

--- ruz_gutierrez_Ex04.py
+++ ruz_gutierrez_Ex04.py
@@ -11,13 +11,13 @@ import numpy as np
 
 #def 
 
-img = cv2.imread("p1.jpg", 0)
-ret, thresh = cv2.threshold(img, 127, 255, 0)
-# contours = cv2.findContours(thresh, 1, 2)
+bw_img = cv2.imread("p3.png", 0)
+out = cv2.imread("p3.png")
+
+ret, thresh = cv2.threshold(bw_img, 127, 255, 0)
 image, contours, hierarchy = cv2.findContours(thresh, 1, 2)
 
-
-cnt = contours[0]
+cnt = contours[1]
 M = cv2.moments(cnt)
 
 cx = int(M['m10']/M['m00'])
@@ -33,11 +33,12 @@ print(area)
 print(perimeter)
 print(approx)
 
-img = cv2.drawContours(img, contours, -1, (0, 255, 0), 3)
+bw_img = cv2.drawContours(bw_img, contours, -1, (0, 255, 0), 3)
 
-cv2.imshow("Original", img)
+cv2.imshow("Original", bw_img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
+cv2.imwrite("out.jpg", bw_img)
 
 '''
 
@@ -45,5 +46,14 @@ ERROR LIST:
 
 	Line 26: cv2.error: OpenCV(3.4.2) /io/opencv/modules/imgproc/src/shapedescr.cpp:272: error: (-215:Assertion failed) npoints >= 0 && (depth == 5 || depth == 4) in function 'contourArea'
 		> fixed 1710H, 9-13-18. Reference: https://opencv-python-tutroals.readthedocs.io/en/latest/py_tutorials/py_imgproc/py_contours/py_contours_begin/py_contours_begin.html#contours-getting-started
+	
+	Error as of 2204H:
+		Traceback (most recent call last):
+  		File "ruz_gutierrez_Ex04.py", line 23, in <module>
+    		cx = int(M['m10']/M['m00'])
+		ZeroDivisionError: float division by zero
+
+		Notes: naggaganito lang siya pag yung p3.png yung ipapasa na image. 
+
 
 '''
